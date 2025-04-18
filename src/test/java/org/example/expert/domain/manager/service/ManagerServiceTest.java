43610ae91a0d9dd1bd8b7b1971d8408e6b09ccb5
soonch67890 <soonch67890@gmail.com>
2025-04-18 17:46:32 +0900

    Lv3-2 테스트코드 caes-1 Fix : 테스트코드 패키지 .domain.manager.service.ManagerServiceTest -> Rename : ( manager_목록_조회_시_Todo가_없다면_IRE_에러를_던진다() , assertEquals() : Manager not found -> Todo not found )

--- src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
+++ src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
@@ -39,14 +39,16 @@ class ManagerServiceTest {
     private ManagerService managerService;
 
     @Test
-    public void manager_목록_조회_시_Todo가_없다면_NPE_에러를_던진다() {
-        // given
+    public void manager_목록_조회_시_Todo가_없다면_IRE_에러를_던진다() {
+        // given : 빈 목록 생성
         long todoId = 1L;
         given(todoRepository.findById(todoId)).willReturn(Optional.empty());
 
-        // when & then
+        // when : 빈 목록에 조회시 & then : InvalidRequestException 에러를 던진다
         InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> managerService.getManagers(todoId));
-        assertEquals("Manager not found", exception.getMessage());
+        assertEquals("Todo not found", exception.getMessage());
+        // "Manager not found" -> "Todo not found" 수정
+
     }
 
     @Test
