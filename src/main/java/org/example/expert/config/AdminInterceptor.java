bf2a5a16a74a00f502c48237d9a024cf5fbc84d9
soonch67890 <soonch67890@gmail.com>
2025-04-21 11:01:36 +0900

    LV4 API 로깅 Feat : admin 접근 시간 표기

--- src/main/java/org/example/expert/config/AdminInterceptor.java
+++ src/main/java/org/example/expert/config/AdminInterceptor.java
@@ -2,6 +2,7 @@ package org.example.expert.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.auth.exception.AccessDeniedException;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -18,6 +19,12 @@ public class AdminInterceptor implements HandlerInterceptor {
         if(!(role instanceof UserRole && role == UserRole.ADMIN)) {
             throw new AccessDeniedException("관리자만 접근할 수 있습니다.");
         }
+
+        String username = (String) request.getSession().getAttribute("username"); // 세션에 저장된 사용자명
+        String ip = request.getRemoteAddr();
+        LocalDateTime now = LocalDateTime.now();
+
+        log.info("관리자 접속 - 사용자명: {}, IP: {}, 시간: {}", username, ip, now);
 
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
